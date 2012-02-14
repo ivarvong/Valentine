61800ee1fb69deac4dba86c4b9702be1937d8127
Ivar Vong <ivar@ivarvong.com>
2012-02-13 18:01:10 -0800

    pretty much working. hash out and in. needs to be prettier

--- js/app.js
+++ js/app.js
@@ -1,71 +1,82 @@
+
+function userDraw() {
+
+	//set it true on mousedown
+	$("#canvas").mousedown(function(){
+		draw=true; 
+		pointList.push(["down"]);
+		ctx.beginPath();
+		isFirst = true;
+	});
+	
+	//reset it on mouseup
+	$("#canvas").mouseup(function(){ 
+		draw=false; 
+		pointList.push(["up"]);
+		window.location.hash = JSON.stringify(pointList); //set hash
+		return false; //disables browser anchor jump behavior
+	});
+	
+	$("#canvas").mousemove(function(e) {
+		if(draw == true){
+				pointList.push([e.pageX,e.pageY]);
+				
+				if (isFirst == true) {
+					ctx.moveTo(e.pageX,e.pageY);
+					isFirst = false;
+				} else {
+					ctx.lineTo(e.pageX,e.pageY);
+				}
+				
+				ctx.stroke();
+				
+				console.log(JSON.stringify(pointList));
+		}    
+	});
+}
+
+function drawBot() {
+	var todo = JSON.parse(window.location.hash.substring(1));
+	var i = 0;
+	var drawBot = window.setInterval(function() {
+		var k = todo[i];
+		//console.log(todo.length - i);
+		if (k[0] == "down") {
+			ctx.beginPath();
+			ctx.moveTo(todo[i+1][0],todo[i+1][1]);
+			i++;
+		} else if (k[0] == "up") {
+			//pass
+			i++;
+		} else {
+			ctx.lineTo(todo[i][0],todo[i][1]);
+			ctx.stroke();
+			i++;
+		}
+		if (i >= todo.length) {
+			window.clearInterval(drawBot);
+		}
+	}, 50);
+}
+
 $(document).ready(function() {
-        var draw = false;
-        var pointList = new Array();
-        var isFirst = true;
-       
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.strokeStyle = '#CD1076';
 
-        //set it true on mousedown
-        $("#canvas").mousedown(function(){
-        	draw=true; 
-        	pointList.push(["down"]);
-        	ctx.beginPath();
-        	isFirst = true;
-        });
+	draw = false;
+	pointList = new Array();
+	isFirst = true;
+	
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	ctx.strokeStyle = '#CD1076';
+	ctx.lineWidth = 3;
+	ctx.lineCap = "round";
+	
 
-        //reset it on mouseup
-        $("#canvas").mouseup(function(){ 
-        	draw=false; 
-        	pointList.push(["up"]);
-		});
-
-        $("#canvas").mousemove(function(e) {
-            if(draw == true){
-            		pointList.push([e.pageX,e.pageY]);
-                    ctx.lineWidth = 3;
-                    ctx.lineCap = "round";
-                    
-                    if (isFirst == true) {
-	                    ctx.moveTo(e.pageX,e.pageY);
-	                    isFirst = false;
-					} else {
-	                    ctx.lineTo(e.pageX,e.pageY);
-					}
-					
-                    ctx.stroke();
-                    
-                    console.log(JSON.stringify(pointList));
-            }    
-       });
-
-        
-       //code for color pallete
-        $("#clr > div").click(
-        function(){
-               ctx.strokeStyle = $(this).css("background-color");
-        });
-     
-        //Eraser
-        $("#eraser").click(function(){
-        	ctx.strokeStyle = '#fff';
-        });
-
-        //Code for save the image
-        $("#save").click(function(){ 
-            $("#result").append("<br /><br /><img src="+
-            canvas.toDataURL()+ 
-           " /><br /><a href="+canvas.toDataURL()+ 
-           " target='_blank'>show</a>");
-        });
-     
-        //Clear 
-        $("#clear").click(function(){
-                 ctx.fillStyle = "#fff";
-                 ctx.fillRect(0, 0, canvas.width, canvas.height);
-                 ctx.strokeStyle = "red";
-                 ctx.fillStyle = "red";
-            }
-        );
+	if(window.location.hash) {
+	  // Fragment exists
+	  drawBot();
+	} else {
+	  // Fragment doesn't exist
+	  userDraw();
+	}        
 });
