ff347ce7c28fbef783e16ce8aa50e8dfca464f83
Ivar Vong <ivar@ivarvong.com>
2012-02-13 17:32:04 -0800

    paths instead of points. progress

--- js/app.js
+++ js/app.js
@@ -1,25 +1,42 @@
 $(document).ready(function() {
         var draw = false;
+        var pointList = new Array();
+        var isFirst = true;
        
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.strokeStyle = '#CD1076';
 
         //set it true on mousedown
-        $("#canvas").mousedown(function(){draw=true;});
+        $("#canvas").mousedown(function(){
+        	draw=true; 
+        	pointList.push(["down"]);
+        	ctx.beginPath();
+        	isFirst = true;
+        });
 
         //reset it on mouseup
-        $("#canvas").mouseup(function(){draw=false;});
+        $("#canvas").mouseup(function(){ 
+        	draw=false; 
+        	pointList.push(["up"]);
+		});
 
         $("#canvas").mousemove(function(e) {
             if(draw == true){
+            		pointList.push([e.pageX,e.pageY]);
                     ctx.lineWidth = 3;
                     ctx.lineCap = "round";
-                    ctx.beginPath();
-                    ctx.moveTo(e.pageX,e.pageY);
-                    ctx.lineTo(e.pageX+1,e.pageY+1);
+                    
+                    if (isFirst == true) {
+	                    ctx.moveTo(e.pageX,e.pageY);
+	                    isFirst = false;
+					} else {
+	                    ctx.lineTo(e.pageX,e.pageY);
+					}
+					
                     ctx.stroke();
                     
+                    console.log(JSON.stringify(pointList));
             }    
        });
 
